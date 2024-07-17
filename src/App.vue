626b27ada96f6215c752b9b4a17e60f75f760d76
bek001rasulov <tarhushrasulov@gmail.com>
2024-07-17 18:59:29 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { CheckCircleIcon } from "@heroicons/vue/24/solid";
+import {
+  ClockIcon,
+  ListBulletIcon,
+  ChartBarIcon,
+} from "@heroicons/vue/24/outline";
+
+const navItems = ["timeline", "activities", "progress"];
+</script>
+
+<template>
+  <header
+    class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3"
+  >
+    <a href="#">
+      <img src="./assets/vue.svg" alt="Logo" class="h-9" />
+    </a>
+    <a href="#" class="text-sm">
+      <div v-if="true" class="flex items-center gap-1">
+        Day complete!
+        <CheckCircleIcon class="h-7 text-green-500" />
+      </div>
+      <div v-else class="flex items-center gap-1">
+        <div>Progress: <span class="font-mono">20%</span></div>
+        <div class="h-3 w-3 rounded-full bg-red-500"></div>
+      </div>
+    </a>
+  </header>
+
+  <main class="flex flex-grow flex-col"></main>
+
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li v-for="page in navItems" :key="page" class="flex-1">
+        <a v-bind:href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
+          <ClockIcon v-if="page === 'timeline'" class="w-6 h-6" />
+          <ListBulletIcon v-else-if="page === 'activities'" class="w-6 h-6" />
+          <ChartBarIcon v-else class="w-6 h-6" />{{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
