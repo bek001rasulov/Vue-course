50b3046efc8a596a9732e8bb081984491eb5646f
bek001rasulov <tarhushrasulov@gmail.com>
2024-08-06 16:17:28 +0500

    13 urok

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Timeline</h1>
+  <div>
+    <ul>
+      <li
+        v-for="i in [0, 1, 2, 3, 4, 5]"
+        class="relative flex flex-col items-center gap-2 border-b border-gray-200 py-10 px-4"
+      >
+        <a
+          href="#"
+          class="absolute -bottom-4 rounded bg-gray-100 px-2 font-mono text-lg text-gray-500"
+          >{{ i }}:00</a
+        >
+      </li>
+    </ul>
+  </div>
 </template>
