b923e44cd390e7d16ea5c769f7927239999cf7cc
bek001rasulov <tarhushrasulov@gmail.com>
2024-08-06 15:51:13 +0500

    11 urok

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,5 +1,8 @@
+<script setup>
+import {PAGE_TIMELINE} from "../constants.js";
+</script>
 <template>
-  <a href="#">
+  <a :href="`#${PAGE_TIMELINE}`">
     <img src="../assets/vue.svg" alt="Logo" class="h-9" />
   </a>
 </template>
